7e6bfd1b994ac606a68fafcfc2d450b7724a3b9d
Dushanka Perera <dushankadev@gmail.com>
2022-01-28 13:56:18 +0000

    Connect html pages to url to test html rendering

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_social(request):
-    return HttpResponse('Hello Social Page')
+    # return HttpResponse('Hello Social Page')
+    return render(request, 'userprofile.html')
