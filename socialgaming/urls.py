7d6214d85b8bcb1c004924a58701ae884d392bf1
Dushanka Perera <dushankadev@gmail.com>
2022-01-30 15:06:10 +0000

    Connect allauth and update urls

--- socialgaming/urls.py
+++ socialgaming/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('socialnetwork.urls')),
+    path('accounts/', include('allauth.urls')),
     path('socialprofile/', include('socialnetwork.urls')),
 ]
