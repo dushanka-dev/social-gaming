ed3d67a710dfafb8e1e87bb15dba25202e2d073b
Dushanka Perera <dushankadev@gmail.com>
2022-02-04 18:00:40 +0000

    Update UserProfile class with user info fields

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    """Create user profile models"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=200, blank=True)
+    bio = models.TextField(max_length=500)
+    country = models.CharField(max_length=200, blank=True)
