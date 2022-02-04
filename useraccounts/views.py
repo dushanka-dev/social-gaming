0dda837814d7cab7dc7eac0d603015e42de38edd
Dushanka Perera <dushankadev@gmail.com>
2022-02-04 14:14:53 +0000

    Add doc string to user_profile

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def user_profile(request):
+    """render user profile"""
     context = {
         'user': request.user
     }
-
     return render(request, 'userprofile.html', context)
