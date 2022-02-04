d8dc49c6752c1d75633b1a78d58b7f6d214d65c1
Dushanka Perera <dushankadev@gmail.com>
2022-02-04 18:09:33 +0000

    Create user picture and friends models

--- profiles/models.py
+++ profiles/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    """Create user profile models"""
+    """User profile section"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
     email = models.EmailField(max_length=200, blank=True)
     bio = models.TextField(max_length=500)
     country = models.CharField(max_length=200, blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+class UserPicture(models.Model):
+    """Upload User Profile picture to profile"""
+    user_picture = models.ImageField(default='default-pic.png', upload_to='uploads/')
+
+class Friends(models.Model):
+    """Upload User Profile picture to profile"""
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
