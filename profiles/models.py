61a1914ca756839fff04de7ab6bc7b915305a2ea
Dushanka Perera <dushankadev@gmail.com>
2022-02-05 14:29:39 +0000

    Import cloudinary models, comment for testing. Default img bug.

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     """User profile section"""
@@ -7,10 +8,16 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
     email = models.EmailField(max_length=200, blank=True)
-    bio = models.TextField(max_length=500)
+    bio = models.TextField(max_length=500, blank=True)
     country = models.CharField(max_length=200, blank=True)
     slug = models.SlugField(unique=True, blank=True)
-    user_picture = models.ImageField(default='default-pic.png', upload_to='uploads/')
+    # user_picture = CloudinaryField(default='uploads/default-image.png')
+    user_picture = models.ImageField(default='default-image.png', upload_to='media/uploads/')
+    # updated = models.DateTimeField(auto_now=True)
+    # created = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return f"{self.user.username}-{self.created}"
 
 class Friend(models.Model):
     """Upload User Profile picture to profile"""
