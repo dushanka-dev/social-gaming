25b82603b4627867c17d1de61cba5322bffac99d
Dushanka Perera <dushankadev@gmail.com>
2022-02-06 21:36:51 +0000

    Update url path to profiles.urls

--- socialgaming/urls.py
+++ socialgaming/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('socialnetwork.urls')),
     path('accounts/', include('allauth.urls')),
-    path('', include('useraccounts.urls'), name='profile'),
+    path('', include('profiles.urls'), name='profile'),
 ]
