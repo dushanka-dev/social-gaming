2e83ba9641a2d0debc22f8b3b33e19ef354e3090
Dushanka Perera <dushankadev@gmail.com>
2022-02-06 16:38:23 +0000

    Use slugify to display users name as slug. Create profile created var

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.template.defaultfilters import slugify
+# from django.urls import reverse
 # from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
@@ -12,12 +15,17 @@ class UserProfile(models.Model):
     country = models.CharField(max_length=200, blank=True)
     slug = models.SlugField(unique=True, blank=True)
     # user_picture = CloudinaryField(default='uploads/default-image.png')
-    user_picture = models.ImageField(default='media/uploads/default.png', upload_to='uploads/', null=True, blank=True)
+    user_picture = models.ImageField(default='uploads/default.png', upload_to='uploads/', blank=True)
     # updated = models.DateTimeField(auto_now=True)
-    # created = models.DateTimeField(auto_now_add=True)
+    profile_created = models.DateField(default=timezone.now)
 
-    # def __str__(self):
-    #     return f"{self.user.username}-{self.created}"
+    def __str__(self):
+        return f'{self.user}-{self.profile_created}'
+    
+    def save(self, *args, **kwargs):
+        
+        self.slug = slugify(self.user)
+        super().save(*args, **kwargs)
 
 class Friend(models.Model):
     """Upload User Profile picture to profile"""
