d07ea8f45f7f66b7e42c0bb0d35e7da3a5357372
Dushanka Perera <dushankadev@gmail.com>
2022-02-06 21:36:10 +0000

    Add user profiles function with context to render html file

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_profile(request):
+    """render user profile"""
+    context = {
+        'user': request.user
+    }
+    return render(request, 'userprofile.html', context)
