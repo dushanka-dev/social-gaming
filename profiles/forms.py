f7b59774dfc76ca85ad9c575d5b3742d20cde467
Dushanka Perera <dushankadev@gmail.com>
2022-02-09 14:18:02 +0000

    Create class get user profile data in each obj field

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import UserProfile
+
+class ProfileForm(forms.ModelForm):
+    """User profile form to allow users to update their profile fields"""
+    class Meta:
+        """Get user data"""
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'email', 'bio', 'favourite_game', 'user_picture')
