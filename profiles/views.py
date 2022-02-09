a0787e4a9f6f8ab3bee299b0f46ddd2ee963c910
Dushanka Perera <dushankadev@gmail.com>
2022-02-09 15:36:51 +0000

    Display user data in form. Add instance to identify profile.

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import UserProfile
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -7,7 +8,10 @@ def user_profile(request):
     """User profile views with objects"""
 
     users_profile = UserProfile.objects.get(user=request.user)
+    profile_form = ProfileForm(request.POST or None, request.FILES or None, instance=users_profile)
+
     context = {
         'users_profile': users_profile,
+        'profile_form': profile_form,
     }
     return render(request, 'profiles/userprofile.html', context)
