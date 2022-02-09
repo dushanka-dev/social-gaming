2426bb7060af864ca25a23266a8fc35bf141f44b
Dushanka Perera <dushankadev@gmail.com>
2022-02-09 15:48:53 +0000

    Add profile form validation if statement

--- profiles/views.py
+++ profiles/views.py
@@ -9,9 +9,15 @@ def user_profile(request):
 
     users_profile = UserProfile.objects.get(user=request.user)
     profile_form = ProfileForm(request.POST or None, request.FILES or None, instance=users_profile)
+    form_update = False
+
+    if request.method == 'POST' and profile_form.is_valid():
+        profile_form.save()
+        form_update = True
 
     context = {
         'users_profile': users_profile,
         'profile_form': profile_form,
+        'form_update': form_update,
     }
     return render(request, 'profiles/userprofile.html', context)
