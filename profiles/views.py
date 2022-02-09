ab97c858382e393051bdb525e0091de8391d3cfa
Dushanka Perera <dushankadev@gmail.com>
2022-02-09 19:04:09 +0000

    Add if statement to check if user upload image

--- profiles/views.py
+++ profiles/views.py
@@ -10,14 +10,22 @@ def user_profile(request):
     users_profile = UserProfile.objects.get(user=request.user)
     profile_form = ProfileForm(request.POST or None, request.FILES or None, instance=users_profile)
     form_update = False
+    image_update = False
 
     if request.method == 'POST' and profile_form.is_valid():
         profile_form.save()
         form_update = True
 
+    if request.FILES:
+        profile_form.save()
+        form_update = False
+        image_update = True
+        print("Image done!")
+
     context = {
         'users_profile': users_profile,
         'profile_form': profile_form,
         'form_update': form_update,
+        'image_update': image_update,
     }
     return render(request, 'profiles/userprofile.html', context)
