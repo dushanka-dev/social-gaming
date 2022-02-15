275289c2ffe8cdf47de09fffc64c168bc278e199
Dushanka Perera <dushankadev@gmail.com>
2022-02-15 18:35:29 +0000

    Add form valid function using django messages. Show user success msg

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.views.generic.list import ListView
+from django.contrib import messages
+# from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 # from django.views.generic.edit import UpdateView
 from .models import UserProfile
-from .forms import ProfileForm
+# from .forms import ProfileForm
 
 # Create your views here.
 
@@ -13,14 +14,18 @@ class UpdateProfile(UpdateView):
     """Update Profile Model objects to display in template and POST data back to db"""
     model = UserProfile
     # form_class = ProfileForm
-    fields = ['first_name', 'last_name', 'email', 'bio', 'favourite_game']
+    fields = ['first_name', 'last_name', 'email', 'bio', 'favourite_game', 'user_picture']
     template_name = 'profiles/userprofile.html'
     success_url = 'profile'
+    success_message = 'Your Profile Updated Successfully!'
 
     def get_object(self, queryset=None):
         obj = obj = get_object_or_404(UserProfile, user=self.request.user)
         return obj
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Your Profile Updated Successfully!')
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('profile')
-
