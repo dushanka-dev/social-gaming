3ecdcb8ce42cf4cfa41d70144c6354411a951c3d
Dushanka Perera <dushankadev@gmail.com>
2022-02-17 14:04:55 +0000

    Update post model and create datetimefield

--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -10,7 +10,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=True)
     body = models.TextField(max_length=800, blank=True)
-    post_created = models.DateField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now=True)
+    post_date = models.DateField(default=timezone.now)
 
     def __str__(self):
         return f'{self.author}-{self.title}'
