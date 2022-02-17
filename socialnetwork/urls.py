bc15a2f5622597e02fbc3b45e199918124212952
Dushanka Perera <dushankadev@gmail.com>
2022-02-17 14:05:53 +0000

    Connect userpostview url path

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import UserPostsView
 
 # URL Conf
 urlpatterns = [
     path('', views.home, name='home'),
-    path('posts/', views.post, name='post'),
+    path('posts/', UserPostsView.as_view(), name='post'),
 ]
