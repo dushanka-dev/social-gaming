d1876a4a3f173b91d8cac253fd547eb7cfebfaf9
Dushanka Perera <dushankadev@gmail.com>
2022-02-17 17:45:45 +0000

    Add time filter to posts. Create code to display only users posts

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+# from django.contrib.auth.models import User
+# from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Post
+# from profiles.models import UserProfile
 
 
 # def hello_social(response):
@@ -16,7 +19,21 @@ def home(response):
 #     return render(response, 'socialnetwork/posts.html', {})
 
 
+class PostsView(ListView):
+    """Display all posts"""
+    model = Post
+    template_name = 'socialnetwork/posts.html'
+    ordering = ['-created_time']
+
+
 class UserPostsView(ListView):
     """Display users posts"""
     model = Post
     template_name = 'socialnetwork/my-posts.html'
+    ordering = ['-created_time']
+
+    def get_queryset(self):
+        """Display users posts"""
+        users_posts = Post.objects.filter(author=self.request.user)
+        return users_posts
+        
