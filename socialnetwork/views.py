1eda9769c658f1d895d406594bfc497b988dc69a
Dushanka Perera <dushankadev@gmail.com>
2022-02-17 14:08:22 +0000

    Import listview. Create userpostview, connect model and html

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
+from .models import Post
 
 
 # def hello_social(response):
 #     return render(response, 'base.html', {})
 
 def home(response):
+    """Landing page view"""
     return render(response, 'socialnetwork/landing.html', {})
 
-def post(response):
-    return render(response, 'socialnetwork/posts.html', {})
+# def post(response):
+#     """Landing page view"""
+#     return render(response, 'socialnetwork/posts.html', {})
+
+
+class UserPostsView(ListView):
+    """Display users posts"""
+    model = Post
+    template_name = 'socialnetwork/posts.html'
