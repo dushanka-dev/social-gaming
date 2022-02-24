fc705d33a792b476da1ddb03e465b5a970c77da9
Dushanka Perera <dushankadev@gmail.com>
2022-02-24 22:56:37 +0000

    Add url path to comments page with view

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostsView, UserPostsView, EditPosts, DeletePost, Likes
+from .views import PostsView, UserPostsView, EditPosts, DeletePost, Likes, CreateComment
 
 # URL Conf
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('posts/', PostsView.as_view(), name='posts'),
     path('myposts/', UserPostsView.as_view(), name='my-posts'),
     path('myposts/<int:pk>/likes', Likes.as_view(), name='likes'),
+    path('comment/<int:pk>/', CreateComment.as_view(), name='comment'),
     path('editposts/<int:pk>/', EditPosts.as_view(), name='edit-posts'),
     path('deleteposts/<int:pk>/', DeletePost.as_view(), name='delete-posts'),
 ]
