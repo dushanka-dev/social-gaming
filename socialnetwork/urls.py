c56721ac18ff818eb566747a097cfe4f8a363954
Dushanka Perera <dushankadev@gmail.com>
2022-02-25 20:40:17 +0000

    Reorder urls. Add delete comment url

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
-from .views import PostsView, UserPostsView, EditPosts, DeletePost, Likes, CreateComment, EditComment
+from .views import PostsView, UserPostsView, EditPosts, DeletePost, Likes, CreateComment, EditComment, DeleteComment
 
 # URL Conf
 urlpatterns = [
     path('', views.home, name='home'),
-    path('posts/', PostsView.as_view(), name='posts'),
     path('myposts/', UserPostsView.as_view(), name='my-posts'),
     path('myposts/<int:pk>/likes', Likes.as_view(), name='likes'),
-    path('comment/<int:pk>/', CreateComment.as_view(), name='comment'),
-    path('editcomment/<int:pk>/', EditComment.as_view(), name='edit-comment'),
+    path('posts/', PostsView.as_view(), name='posts'),
     path('editposts/<int:pk>/', EditPosts.as_view(), name='edit-posts'),
     path('deleteposts/<int:pk>/', DeletePost.as_view(), name='delete-posts'),
+    path('comment/<int:pk>/', CreateComment.as_view(), name='comment'),
+    path('editcomment/<int:pk>/', EditComment.as_view(), name='edit-comment'),
+    path('deletecomment/<int:pk>/', DeleteComment.as_view(), name='delete-comment'),
 ]
