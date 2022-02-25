8e3100f220a91d7ed110b74fed394439b3689dc3
Dushanka Perera <dushankadev@gmail.com>
2022-02-25 18:53:50 +0000

    Create edit comment url path with pk

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostsView, UserPostsView, EditPosts, DeletePost, Likes, CreateComment
+from .views import PostsView, UserPostsView, EditPosts, DeletePost, Likes, CreateComment, EditComment
 
 # URL Conf
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('myposts/', UserPostsView.as_view(), name='my-posts'),
     path('myposts/<int:pk>/likes', Likes.as_view(), name='likes'),
     path('comment/<int:pk>/', CreateComment.as_view(), name='comment'),
+    path('editcomment/<int:pk>/', EditComment.as_view(), name='edit-comment'),
     path('editposts/<int:pk>/', EditPosts.as_view(), name='edit-posts'),
     path('deleteposts/<int:pk>/', DeletePost.as_view(), name='delete-posts'),
 ]
