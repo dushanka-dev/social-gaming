09931181a62dbd539ec7d1b90f520ac57a370b8e
Dushanka Perera <dushankadev@gmail.com>
2022-02-25 18:58:18 +0000

    Add loadstatic to top of page. Remove unused code

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import UserProfile, Friend
+from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def user_profile_created(sender, instance, created, **kwargs):
@@ -10,14 +10,14 @@ def user_profile_created(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=Friend)
-def add_friend_created(sender, instance, created, **kwargs):
-    """ Listen to user profile been created through signal """
+# @receiver(post_save, sender=Friend)
+# def add_friend_created(sender, instance, created, **kwargs):
+#     """ Listen to user profile been created through signal """
 
-    send_friend_invite = instance.friend_sender
-    recieve_friend_invite = instance.friend_receiver
-    if instance.friend_status == 'accepted':
-        send_friend_invite.friends.add(recieve_friend_invite.user)
-        recieve_friend_invite.friends.add(send_friend_invite.user)
-        send_friend_invite.save()
-        recieve_friend_invite.save()
+#     send_friend_invite = instance.friend_sender
+#     recieve_friend_invite = instance.friend_receiver
+#     if instance.friend_status == 'accepted':
+#         send_friend_invite.friends.add(recieve_friend_invite.user)
+#         recieve_friend_invite.friends.add(send_friend_invite.user)
+#         send_friend_invite.save()
+#         recieve_friend_invite.save()
